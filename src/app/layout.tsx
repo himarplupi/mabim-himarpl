26249530e4440fe6977bb541dd4d80a367a04efc
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-10-06 07:04:38 +0700

    chore: add eslint plugins and fix all code style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import "@/styles/globals.css";
-
-import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+
+import "@/styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
