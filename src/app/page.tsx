48bfa22aacb101c1e01bb15e769f2505b0faaf06
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-10-07 21:46:05 +0700

    feat: add the correct countdown dates data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,10 @@ import { SparklesText } from "@/components/ui/sparkles-text";
 export default function HomePage() {
   const tabData = [
     { label: "TM", date: "2024-10-19" },
-    { label: "Day-1", date: "2024-10-20" },
-    { label: "Day-2", date: "2024-10-21" },
-    { label: "Day-3", date: "2024-10-22" },
-    { label: "Day-4", date: "2024-10-23" },
+    { label: "Day-1", date: "2024-10-26" },
+    { label: "Day-2", date: "2024-11-02" },
+    { label: "Day-3", date: "2024-11-03" },
+    { label: "Day-4", date: "2024-11-09" },
   ];
 
   return (
