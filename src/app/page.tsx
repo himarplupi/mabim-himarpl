9279097f5c1ea30188c8ee89bb05703b0d489fea
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-10-08 21:22:24 +0700

    feat: add responsive layout in hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ export default function HomePage() {
   return (
     <main
       id="main"
-      className="relative min-h-screen overflow-hidden bg-[length:512px_512px] bg-repeat xl:max-h-[768px] xl:min-h-fit"
+      className="relative min-h-screen overflow-hidden bg-[length:512px_512px] bg-repeat"
       style={{
         backgroundImage: "url(./assets/main-pattern.webp)",
       }}
@@ -106,20 +106,20 @@ export default function HomePage() {
         </section>
       </div>
 
-      <div className="absolute top-[650px] z-[5] h-64 w-[512px] scale-150">
+      <div className="absolute top-[650px] z-[5] h-64 w-[512px] scale-150 sm:w-full md:top-[550px]">
         <BlurFade delay={0.25 * 8} inView className="relative h-full w-full">
           {/* eslint-disable-next-line  */}
           <Image src={Vector1} alt="Vector 1" fill />
         </BlurFade>
       </div>
 
-      <div className="absolute left-0 right-0 top-[825px] z-[10] opacity-75">
+      <div className="absolute left-0 right-0 top-[825px] z-[10] opacity-75 sm:top-[850px] md:top-[768px] lg:top-[825px]">
         <Hieroglyph length={32} className="text-primary-950" />
       </div>
 
-      <Wave className="absolute -left-32 top-10 opacity-50" />
+      <Wave className="absolute -left-8 top-10 opacity-50 sm:left-0 sm:w-full md:top-10" />
 
-      <Wave className="absolute -left-32 top-[700px] rotate-180 opacity-50" />
+      <Wave className="absolute -left-8 top-[700px] rotate-180 opacity-50 sm:left-0 sm:w-full md:top-[700px]" />
 
       <div className="bg-primary-700/95 pt-96">
         <KelompokSection />
