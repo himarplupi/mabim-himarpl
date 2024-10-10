54b4361cad6acbaf8fb168aafbfeb6b0b4917053
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-10-10 08:43:32 +0700

    feat(posthog): remove page view false

--- src/components/providers/posthog.tsx
+++ src/components/providers/posthog.tsx
@@ -8,8 +8,6 @@ if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: "/ingest",
     ui_host: "https://us.posthog.com",
-    capture_pageview: false,
-    capture_pageleave: true,
   });
 }
 
