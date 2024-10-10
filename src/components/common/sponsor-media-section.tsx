e03b098e9aa3ec126ac95de8a051a5a74bc52fe7
Arsya <azelord18@gmail.com>
2024-10-10 21:22:17 +0700

    fix: adjust sponsor logo sizes and add new logos

--- src/components/common/sponsor-media-section.tsx
+++ src/components/common/sponsor-media-section.tsx
@@ -4,38 +4,41 @@ import { BlurFade } from "@/components/ui/blur-fade";
 import { Marquee } from "@/components/ui/marquee";
 import LogoAlloBank from "@/assets/sponsors/LogoAlloBank.png";
 import LogoKahf from "@/assets/sponsors/LogoKahf.png"
+import LogoRuru from "@/assets/sponsors/LogoRuru.jpg"
+import LogoMenyala from "@/assets/sponsors/LogoMenyala.png"
 
 
 const sponsorMediaPartners = [
   {
-    name: "PT. PT Paragon Technology and Innovation",
+    name: "PT Paragon Technology and Innovation",
     logo: LogoKahf,
   },
   {
-    name: "PT. PT Allo Bank Indonesia Tbk",
+    name: "PT Allo Bank Indonesia Tbk",
     logo: LogoAlloBank,
   },
   {
-    name: "PT. GHI",
-    logo: null,
+    name: "Menyala by OCBC",
+    logo: LogoRuru,
   },
   {
-    name: "PT. JKL",
-    logo: null,
-  },
-  {
-    name: "PT. MNO",
-    logo: null,
+    name: "Ruru Snack",
+    logo: LogoMenyala,
   },
 ];
 
 const firstRow = sponsorMediaPartners.slice(0, sponsorMediaPartners.length / 2);
 const secondRow = sponsorMediaPartners.slice(sponsorMediaPartners.length / 2);
 
-const SponsorMediaPartnerCard = ({ logo }: { logo: string | null }) => {
+const SponsorMediaPartnerCard = ({ logo }: { logo: ImageStaticData | null }) => {
   return (
-    <figure className="relative aspect-video h-32 w-64 cursor-pointer overflow-hidden rounded-xl border bg-neutral-400 p-4">
-      {logo && <Image fill alt="" src={logo} />}
+    <figure className="relative aspect-video h full h-32 w-full w-auto cursor-pointer object-contain object-center overflow-hidden flex items-center justify-center rounded-xl p-4">
+      {logo && 
+      <Image 
+        src={logo}
+        alt="Sponsor or Media Partner Logo"
+        style={{ maxHeight: '150%', maxWidth: '100%' }}
+      />}
     </figure>
   );
 };
